76c5511431977849758b08556bc7b521c324a282
WhiteSockLoafer <jose.reverte.cazorla@gmail.com>
2021-05-22 19:49:07 +0200

    Directory reader y conversor a keras visualizer

---
+++ gensim/w2v_visualizer-v2.py
@@ -0,0 +1,48 @@
+"""
+@author: BrikerMan
+@contact: eliyar917@gmail.com
+@blog: https://eliyar.biz
+@version: 1.0
+@license: Apache Licence
+@file: w2v_visualizer.py
+@time: 2017/7/30 上午9:37
+"""
+
+from gensim.models import KeyedVectors
+
+# Load gensim word2vec
+w2v_path = 'modelo/word2vec.wordvectors'
+w2v = KeyedVectors.load(w2v_path)
+
+import io
+
+# Vector file, `\t` seperated the vectors and `\n` seperate the words
+"""
+0.1\t0.2\t0.5\t0.9
+0.2\t0.1\t5.0\t0.2
+0.4\t0.1\t7.0\t0.8
+"""
+out_v = io.open('modelo/vecs.tsv', 'w', encoding='utf-8')
+
+# Meta data file, `\n` seperated word
+"""
+token1
+token2
+token3
+"""
+out_m = io.open('modelo/meta.tsv', 'w', encoding='utf-8')
+
+# Write meta file and vector file
+for index in range(len(w2v.index_to_key)):
+    word = w2v.index_to_key[index]
+    vec = w2v.vectors[index]
+    out_m.write(word + "\n")
+    out_v.write('\t'.join([str(x) for x in vec]) + "\n")
+out_v.close()
+out_m.close()
+
+# Then we can visuale using the `http://projector.tensorflow.org/` to visualize those two files.
+
+# 1. Open the Embedding Projector.
+# 2. Click on "Load data".
+# 3. Upload the two files we created above: vecs.tsv and meta.tsv.
--- gensim/word2vec.py
+++ gensim/word2vec.py
@@ -2,51 +2,40 @@ import os
 import re
 import time
 from gensim.models import Word2Vec
+from nltk.corpus import stopwords
 
 
-def preprocessing(file: str):
+class DirectoryReader(object):
+    def __init__(self, dirname):
+        self.dirname = dirname
 
-    if not os.path.isfile(file):
-        print('File not found')
-        return
-
-    try:
-        f = open(file)
-    except IOError:
-        print('File not accesible')
-        return
-
-    content = f.read()
-    # Eliminar comas
-    content = re.sub(r'\,', '', content)
-    # Sustituir caracteres que no sean del alfabeto o espacios por saltos de linea
-    content = re.sub(r'[^a-zA-ZÀ-ÿ\u00f1\u00d1\n ]', '\n', content)
-    # ELiminar uno o más espacios a principio o final de linea
-    content = re.sub(r'^ +| +$', '', content, flags=re.MULTILINE)
-    # Sustituir dos o más saltos de línea seguidos por uno solo
-    content = re.sub(r'\n{2,}', '\n', content)
-    texts = content.split('\n')
-
-    stoplist = set(
-        'el la los las se a o por para sin como su que al en de y del con un una sobre sus lo no cuando'.split())
-
-    sentences = [[word for word in sentence.lower().split() if word not in stoplist]
-                 for sentence in texts if len(sentence.split()) > 2]
-
-    return sentences
+    def __iter__(self):
+        for file_name in os.listdir(self.dirname):
+            for line in open(os.path.join(self.dirname, file_name), encoding='utf-8'):
+                line = preprocess(line)
+                if len(line) > 2:
+                    yield line
 
 
-control = time.time()
-print('Preprocessing your corpus...')
+def preprocess(line: str):
 
-sentences = preprocessing('corpus/constitucion')
-print(f'Time taken : {(time.time() - control) / 60:.2f} mins\n')
+    # ELiminar caracteres que no sean del alfabeto o espacios
+    line = re.sub(r'[^a-zA-ZÀ-ÿ\u00f1\u00d1 ]', '', line)
+
+    stoplist = stopwords.words('spanish') + 'así cada demás mediante ningún si tal'.split()
+
+    return [token for token in line.lower().split() if token not in stoplist and len(token) > 1]
+
+
+
+corpus = DirectoryReader(dirname="corpus")
 
 control = time.time()
 print('Preparing your model...')
 
-model = Word2Vec(sentences=sentences, vector_size=300,
+model = Word2Vec(sentences=corpus, vector_size=300,
                  window=5, min_count=1, workers=4)
+model.wv.save("word2vec.wordvectors")
 print(f'Time taken : {(time.time() - control) / 60:.2f} mins\n')
 
-print(model.wv.most_similar(positive=['derechos']))
+print(model.wv.most_similar(positive=['gobierno']))
