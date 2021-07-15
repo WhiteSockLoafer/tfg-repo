f9c8263e9d97e6e54a74b6551f71d65e13950b5c
WhiteSockLoafer <jose.reverte.cazorla@gmail.com>
2021-07-15 12:07:41 +0200

    gracias bootstrap

--- legal_search/analogy_searcher/views.py
+++ legal_search/analogy_searcher/views.py
@@ -1,16 +1,17 @@
+import os
 from django.shortcuts import render
-from django.template import loader
+from django.conf import settings
 from gensim.models import Word2Vec
 
-model = Word2Vec.load('/home/jrc/Universidad/2020-2021/2_TFG/tfg-repo/legal_search/analogy_searcher/w2v_model/word2vec_8.model')
+model = Word2Vec.load(os.path.join(settings.BASE_DIR, 'analogy_searcher/w2v_model/word2vec_8.model'))
 
 def index(request):
-    return render(request, 'analogy_searcher/index.html')
+    return render(request, 'predict.html')
 
 def predict(request):
     tuples = model.wv.most_similar(negative=[request.POST['n1']], positive=[request.POST['p1'], request.POST['p2']])
     result_list = [t[0] for t in tuples]
-    return render(request, 'analogy_searcher/index.html', 
+    return render(request, 'predict.html', 
     context= {
         "result_list": result_list
     })
