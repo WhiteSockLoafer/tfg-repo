8b4f791d0481dfe7a08c4eb91b3b58751540cfb8
WhiteSockLoafer <jose.reverte.cazorla@gmail.com>
2021-07-16 19:53:00 +0200

    server terminado (en principio)

--- legal_search/analogy_searcher/urls.py
+++ legal_search/analogy_searcher/urls.py
@@ -3,6 +3,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('predict', views.predict, name='predict')
+    path('', views.predict, name='predict')
 ]
--- legal_search/analogy_searcher/views.py
+++ legal_search/analogy_searcher/views.py
@@ -3,15 +3,25 @@ from django.shortcuts import render
 from django.conf import settings
 from gensim.models import Word2Vec
 
-model = Word2Vec.load(os.path.join(settings.BASE_DIR, 'analogy_searcher/w2v_model/word2vec_8.model'))
+model = Word2Vec.load(os.path.join(
+    settings.BASE_DIR, 'analogy_searcher/w2v_model/word2vec_4.model'
+))
 
-def index(request):
-    return render(request, 'predict.html')
 
 def predict(request):
-    tuples = model.wv.most_similar(negative=[request.POST['n1']], positive=[request.POST['p1'], request.POST['p2']])
-    result_list = [t[0] for t in tuples]
-    return render(request, 'predict.html', 
-    context= {
-        "result_list": result_list
-    })
+    if request.method == 'POST':
+        tuples = model.wv.most_similar(negative=[request.POST['n1']], positive=[
+                                       request.POST['p1'], request.POST['p2']], topn=8)
+        final_list = []
+        for t in tuples:
+            final_list.append((t[0], int(t[1] * 100)))
+
+        return render(request, 'post_predict.html', context={
+            "result_list": final_list,
+            "n1": request.POST['n1'],
+            "p1": request.POST['p1'],
+            "p2": request.POST['p2']
+
+        })
+    else:
+        return render(request, 'get_predict.html')
